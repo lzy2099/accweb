dc3eb9d662ed4f5e8c87c9eba6d135f4a8d12c2a
John <lzy2099@163.com>
2019-08-05 13:56:27 -0400

    add change password

--- account/views.py
+++ account/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
 
 def user_login(request):
     if request.method == "POST":
