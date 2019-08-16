f95e8328d7c5c482c6da5a59d7ed683023b2f66f
John <lzy2099@163.com>
2019-08-16 12:16:27 -0400

    添加views 排行

--- images/views.py
+++ images/views.py
@@ -12,7 +12,6 @@ import redis
 from django.conf import settings
 r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
-
 @login_required
 def image_create(request):
     if request.method == "POST":
@@ -28,11 +27,21 @@ def image_create(request):
     else:
         form = ImageCreateForm(data=request.GET)
     return render(request, 'images/image/create.html', {'section': 'images', 'form':form})
+
 @login_required
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     total_views = r.incr('image:{}:views'.format(image.id))
+    r.zincrby('image_ranking', image.id, 1)
     return render(request, 'images/image/detail.html', {'section':'images', 'image':image, 'total_views': total_views})
+
+@login_required
+def image_ranking(request):
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {'section': 'images', 'most_viewed': most_viewed})
 
 @ajax_required
 @login_required
